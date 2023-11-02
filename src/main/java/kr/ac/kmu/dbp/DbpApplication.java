327727517831d0ff7a0901e4937c3f5f460fc257
KDH <kde6571@gmail.com>
2023-11-02 09:50:08 +0900

    Init: Project Initializing

---
+++ src/main/java/kr/ac/kmu/dbp/DbpApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kmu.dbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbpApplication.class, args);
+    }
+
+}
