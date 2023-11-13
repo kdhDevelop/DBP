016144c8474564abb95319bebfea9c70a4ee44ba
KDH <kde6571@gmail.com>
2023-11-13 20:40:28 +0900

    Feat: Add Employee

---
+++ src/main/java/kr/ac/kmu/dbp/entity/employee/employee.java
@@ -0,0 +1,27 @@
+package kr.ac.kmu.dbp.entity.employee;
+
+import kr.ac.kmu.dbp.entity.department.Department;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class employee {
+    private int pid;
+
+    private String account;
+    private String password;
+
+    private String name;
+    private Gender gender;
+
+    private String residentRegistrationNumber;
+    private String phoneNumber;
+
+    private long zipCode;
+    private String address1;
+    private String address2;
+
+    private Role role;
+    private Rank rank;
+}
