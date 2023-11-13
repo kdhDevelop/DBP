a1f64eb2e5d2d31abe63e95e0c06dbc04ff60d83
KDH <kde6571@gmail.com>
2023-11-13 21:11:12 +0900

    Feat: Add getEmployee method in EmployeeDataBaseRepository

--- src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
@@ -1,11 +1,18 @@
 package kr.ac.kmu.dbp.repository.employee;
 
+import kr.ac.kmu.dbp.entity.employee.Employee;
+import kr.ac.kmu.dbp.entity.employee.Gender;
+import kr.ac.kmu.dbp.entity.employee.Rank;
+import kr.ac.kmu.dbp.entity.employee.Role;
 import kr.ac.kmu.dbp.repository.DataBaseConnection;
 import kr.ac.kmu.dbp.repository.Table;
 import kr.ac.kmu.dbp.repository.department.DepartmentDataBaseRepository;
 import kr.ac.kmu.dbp.repository.department.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class EmployeeDataBaseRepository extends Table implements EmployeeRepository {
@@ -21,5 +28,23 @@ public class EmployeeDataBaseRepository extends Table implements EmployeeReposit
     @Override
     protected String getTableCreateQuery() {
         return "CREATE TABLE employee ( pid int NOT NULL AUTO_INCREMENT, account varchar(100), password varchar(1000), name varchar(50), gender varchar(50), residentRegistrationNumber char(14) UNIQUE, phoneNumber char(14), zipCode int unsigned, address1 varchar(1000), address2 varchar(1000), role varchar(100), departmentPid int, rank varchar(100), createDate date, PRIMARY KEY(pid) );";
+    }
+
+    private Employee getEmployee(ResultSet resultSet) throws SQLException {
+        return Employee.builder()
+                .pid(resultSet.getInt("pid"))
+                .account(resultSet.getString("account"))
+                .password(resultSet.getString("password"))
+                .name(resultSet.getString("name"))
+                .gender(Gender.valueOf(resultSet.getString("gender")))
+                .residentRegistrationNumber(resultSet.getString("residentRegistrationNumber"))
+                .phoneNumber(resultSet.getString("phoneNumber"))
+                .zipCode(resultSet.getLong("zipCode"))
+                .address1(resultSet.getString("address1"))
+                .address2(resultSet.getString("address2"))
+                .role(Role.valueOf(resultSet.getString("role")))
+                .department(departmentRepository.readByPid(resultSet.getInt("departmentPid")))
+                .rank(Rank.valueOf(resultSet.getString("rank")))
+                .build();
     }
 }
