00eb24a559a98eb607ca2429e6a63abbf0329534
KDH <kde6571@gmail.com>
2023-11-14 00:10:41 +0900

    Fix: Remove getEmployee method in EmployeeDataBaseRepository

--- src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
@@ -33,24 +33,6 @@ public class EmployeeDataBaseRepository extends Table implements EmployeeReposit
         return "CREATE TABLE employee ( pid int NOT NULL AUTO_INCREMENT, account varchar(100), password varchar(1000), name varchar(50), gender varchar(50), residentRegistrationNumber char(14) UNIQUE, phoneNumber char(14), zipCode int unsigned, address1 varchar(1000), address2 varchar(1000), role varchar(100), departmentPid int, rank varchar(100), PRIMARY KEY(pid) );";
     }
 
-    private Employee getEmployee(ResultSet resultSet) throws SQLException {
-        return Employee.builder()
-                .pid(resultSet.getInt("pid"))
-                .account(resultSet.getString("account"))
-                .password(resultSet.getString("password"))
-                .name(resultSet.getString("name"))
-                .gender(Gender.valueOf(resultSet.getString("gender")))
-                .residentRegistrationNumber(resultSet.getString("residentRegistrationNumber"))
-                .phoneNumber(resultSet.getString("phoneNumber"))
-                .zipCode(resultSet.getLong("zipCode"))
-                .address1(resultSet.getString("address1"))
-                .address2(resultSet.getString("address2"))
-                .role(Role.valueOf(resultSet.getString("role")))
-                .department(departmentRepository.readByPid(resultSet.getInt("departmentPid")))
-                .rank(Rank.valueOf(resultSet.getString("rank")))
-                .build();
-    }
-
     @Override
     public EmployeeDtoDataBaseRepository create(Employee employee) {
         String createQuery = "INSERT INTO employee (password, name, gender, residentRegistrationNumber, phoneNumber, zipCode, address1, address2, role, departmentPid, rank) VALUES ('|=PASSWORD=|', '|=NAME=|', '|=GENDER=|', '|=RESIDENT_REGISTRATION_NUMBER=|', '|=PHONE_NUMBER=|', |=ZIP_CODE=|, '|=ADDRESS_1=|', '|=ADDRESS_2=|', '|=ROLE=|', |=DEPARTMENT_PID=|, '|=RANK=|');"
