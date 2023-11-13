0da91d8eb14905b2d86bbaea7bdc15985a9ad91c
KDH <kde6571@gmail.com>
2023-11-13 20:40:36 +0900

    Feat: Add Department

---
+++ src/main/java/kr/ac/kmu/dbp/entity/department/Department.java
@@ -0,0 +1,13 @@
+package kr.ac.kmu.dbp.entity.department;
+
+import kr.ac.kmu.dbp.entity.employee.employee;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Department {
+    private int pid;
+    private String name;
+    private employee departmentHead;
+}
