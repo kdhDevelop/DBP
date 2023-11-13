36811b6a8c3341ed351dd47dc48267c482b62f27
KDH <kde6571@gmail.com>
2023-11-14 00:29:58 +0900

    Feat: Add readByPid method in DepartmentServiceImpl

--- src/main/java/kr/ac/kmu/dbp/service/department/DepartmentServiceImpl.java
+++ src/main/java/kr/ac/kmu/dbp/service/department/DepartmentServiceImpl.java
@@ -1,5 +1,12 @@
 package kr.ac.kmu.dbp.service.department;
 
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoRepository;
+import kr.ac.kmu.dbp.dto.employee.EmployeeDtoRepository;
+import kr.ac.kmu.dbp.entity.department.Department;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+import kr.ac.kmu.dbp.entity.employee.Gender;
+import kr.ac.kmu.dbp.entity.employee.Rank;
+import kr.ac.kmu.dbp.entity.employee.Role;
 import kr.ac.kmu.dbp.repository.department.DepartmentDataBaseRepository;
 import kr.ac.kmu.dbp.repository.department.DepartmentRepository;
 import kr.ac.kmu.dbp.repository.employee.EmployeeDataBaseRepository;
@@ -17,5 +24,35 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentServiceImpl(DepartmentDataBaseRepository departmentDataBaseRepository, EmployeeDataBaseRepository employeeDataBaseRepository) {
         this.departmentRepository = departmentDataBaseRepository;
         this.employeeRepository = employeeDataBaseRepository;
+    }
+
+    @Override
+    public Department readByPid(int pid) {
+        DepartmentDtoRepository departmentDtoRepository = departmentRepository.readByPid(pid);
+        Department department = Department.builder()
+                .pid(departmentDtoRepository.getPid())
+                .name(departmentDtoRepository.getName())
+                .build();
+
+        EmployeeDtoRepository employeeDtoRepository = employeeRepository.readByPid(departmentDtoRepository.getPid());
+        Employee departmentHead = Employee.builder()
+                .pid(employeeDtoRepository.getPid())
+                .account(employeeDtoRepository.getAccount())
+                .password(employeeDtoRepository.getPassword())
+                .name(employeeDtoRepository.getName())
+                .gender(Gender.valueOf(employeeDtoRepository.getGender()))
+                .residentRegistrationNumber(employeeDtoRepository.getResidentRegistrationNumber())
+                .phoneNumber(employeeDtoRepository.getPhoneNumber())
+                .zipCode(employeeDtoRepository.getZipCode())
+                .address1(employeeDtoRepository.getAddress1())
+                .address2(employeeDtoRepository.getAddress2())
+                .role(Role.valueOf(employeeDtoRepository.getRole()))
+                .department(department)
+                .rank(Rank.valueOf(employeeDtoRepository.getRank()))
+                .build();
+
+        department.setDepartmentHead(departmentHead);
+
+        return department;
     }
 }
