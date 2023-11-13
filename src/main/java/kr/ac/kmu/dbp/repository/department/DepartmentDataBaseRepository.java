68b49e89e224e3b3b84f3f4f2cce079522ec80fc
KDH <kde6571@gmail.com>
2023-11-13 21:30:04 +0900

    Feat: Add EmployeeRepository with EmployeeDataBaseRepository in DepartmentDataBaseRepository

--- src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentDataBaseRepository.java
@@ -2,19 +2,27 @@ package kr.ac.kmu.dbp.repository.department;
 
 import kr.ac.kmu.dbp.repository.DataBaseConnection;
 import kr.ac.kmu.dbp.repository.Table;
+import kr.ac.kmu.dbp.repository.employee.EmployeeDataBaseRepository;
+import kr.ac.kmu.dbp.repository.employee.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class DepartmentDataBaseRepository extends Table implements DepartmentRepository {
 
+    private final EmployeeRepository employeeRepository;
+
     @Autowired
-    public DepartmentDataBaseRepository(DataBaseConnection dataBaseConnection) {
+    public DepartmentDataBaseRepository(DataBaseConnection dataBaseConnection, EmployeeDataBaseRepository employeeRepository) {
         super(dataBaseConnection, "department");
+
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     protected String getTableCreateQuery() {
-        return "CREATE TABLE department ( pid int NOT NULL AUTO_INCREMENT, name varchar(500), departmentHeadPid int, PRIMARY KEY(pid) );";
+        return "CREATE TABLE department ( pid int NOT NULL AUTO_INCREMENT, name varchar(500) UNIQUE, departmentHeadPid int, PRIMARY KEY(pid) );";
     }
+
 }
