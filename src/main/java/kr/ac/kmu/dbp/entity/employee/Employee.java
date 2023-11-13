3c6976355f7929005bf882f4cbf3461941cc05ee
KDH <kde6571@gmail.com>
2023-11-14 00:39:17 +0900

    Feat: Add Constructor for EmployeeDtoRepository in Employee

--- src/main/java/kr/ac/kmu/dbp/entity/employee/Employee.java
+++ src/main/java/kr/ac/kmu/dbp/entity/employee/Employee.java
@@ -1,5 +1,6 @@
 package kr.ac.kmu.dbp.entity.employee;
 
+import kr.ac.kmu.dbp.dto.employee.EmployeeDtoRepository;
 import kr.ac.kmu.dbp.entity.department.Department;
 import lombok.Builder;
 import lombok.Data;
@@ -26,4 +27,24 @@ public class Employee {
     private Rank rank;
 
     private Department department;
+
+    public Employee(EmployeeDtoRepository employeeDtoRepository) {
+        this.pid = employeeDtoRepository.getPid();
+
+        this.account = employeeDtoRepository.getAccount();
+        this.password = employeeDtoRepository.getPassword();
+
+        this.name = employeeDtoRepository.getName();
+        this.gender = Gender.valueOf(employeeDtoRepository.getGender());
+
+        this.residentRegistrationNumber = employeeDtoRepository.getResidentRegistrationNumber();
+        this.phoneNumber = employeeDtoRepository.getPhoneNumber();
+
+        this.zipCode = employeeDtoRepository.getZipCode();
+        this.address1 = employeeDtoRepository.getAddress1();
+        this.address2 = employeeDtoRepository.getAddress2();
+
+        this.role = Role.valueOf(employeeDtoRepository.getRole());
+        this.rank = Rank.valueOf(employeeDtoRepository.getRank());
+    }
 }
