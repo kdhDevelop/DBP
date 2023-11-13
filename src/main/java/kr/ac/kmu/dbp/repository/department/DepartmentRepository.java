a3208f2dd81e7d87fa3c5c46e0c3de44219a8099
KDH <kde6571@gmail.com>
2023-11-13 21:09:21 +0900

    Fix: Change String name to int pid in readByPid method

--- src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentRepository.java
@@ -4,5 +4,5 @@ import kr.ac.kmu.dbp.entity.department.Department;
 
 public interface DepartmentRepository {
     public Department create(Department department);
-    public Department readByPid(String name);
+    public Department readByPid(int pid);
 }
