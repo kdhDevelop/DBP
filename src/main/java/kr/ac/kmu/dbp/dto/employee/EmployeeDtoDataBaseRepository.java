f9c437f7a3e86518ee20b0d95219cf1d2191d560
KDH <kde6571@gmail.com>
2023-11-13 21:46:32 +0900

    Feat: Add EmployeeDtoDataBaseRepository

---
+++ src/main/java/kr/ac/kmu/dbp/dto/employee/EmployeeDtoDataBaseRepository.java
@@ -0,0 +1,49 @@
+package kr.ac.kmu.dbp.dto.employee;
+
+import lombok.Getter;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Getter
+public class EmployeeDtoDataBaseRepository {
+    private int pid;
+
+    private String account;
+    private String password;
+
+    private String name;
+    private String gender;
+    private String residentRegistrationNumber;
+    private String phoneNumber;
+
+    private long zipCode;
+    private String address1;
+    private String address2;
+
+    private String role;
+    private String rank;
+
+    private int departmentPid;
+
+    public EmployeeDtoDataBaseRepository(ResultSet resultSet) throws SQLException {
+        this.pid = resultSet.getInt("pid");
+
+        this.account = resultSet.getString("account");
+        this.password = resultSet.getString("password");
+
+        this.name = resultSet.getString("name");
+        this.gender = resultSet.getString("gender");
+        this.residentRegistrationNumber = resultSet.getString("residentRegistrationNumber");
+        this.phoneNumber = resultSet.getString("phoneNumber");
+
+        this.zipCode = resultSet.getLong("zipCode");
+        this.address1 = resultSet.getString("address1");
+        this.address2 = resultSet.getString("address2");
+
+        this.role = resultSet.getString("role");
+        this.rank = resultSet.getString("rank");
+
+        this.departmentPid = resultSet.getInt("departmentPid");
+    }
+}
