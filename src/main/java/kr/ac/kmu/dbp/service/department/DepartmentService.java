2c85d654b10223435a32d209602dd7520bfa40d0
KDH <kde6571@gmail.com>
2023-11-14 00:24:43 +0900

    Feat: Add readByPid method in DepartmentService

--- src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
+++ src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
@@ -1,4 +1,8 @@
 package kr.ac.kmu.dbp.service.department;
 
+import kr.ac.kmu.dbp.entity.department.Department;
+
 public interface DepartmentService {
+
+    public Department readByPid(int pid);
 }
