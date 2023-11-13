5fc2ae92788e874d6de6755a15d1ba9abefac5f1
KDH <kde6571@gmail.com>
2023-11-13 21:02:10 +0900

    Fix: Remove age column in create query in employee table

--- src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeDataBaseRepository.java
@@ -14,6 +14,6 @@ public class EmployeeDataBaseRepository extends Table implements EmployeeReposit
 
     @Override
     protected String getTableCreateQuery() {
-        return "CREATE TABLE employee ( pid int NOT NULL AUTO_INCREMENT, account varchar(100), password varchar(1000), name varchar(50), age smallint, gender varchar(50), residentRegistrationNumber char(14) UNIQUE, phoneNumber char(14), zipCode int unsigned, address1 varchar(1000), address2 varchar(1000), role varchar(100), departmentPid int, rank varchar(100), createDate date, PRIMARY KEY(pid) );";
+        return "CREATE TABLE employee ( pid int NOT NULL AUTO_INCREMENT, account varchar(100), password varchar(1000), name varchar(50), gender varchar(50), residentRegistrationNumber char(14) UNIQUE, phoneNumber char(14), zipCode int unsigned, address1 varchar(1000), address2 varchar(1000), role varchar(100), departmentPid int, rank varchar(100), createDate date, PRIMARY KEY(pid) );";
     }
 }
