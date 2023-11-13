99e1af530aa105e01215e37524689e2a5a76d10a
KDH <kde6571@gmail.com>
2023-11-14 00:03:25 +0900

    Fix: Change return Employee to EmployeeDtoDataBaseRepository for create method in EmployeeRepository

--- src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeRepository.java
@@ -1,8 +1,9 @@
 package kr.ac.kmu.dbp.repository.employee;
 
+import kr.ac.kmu.dbp.dto.employee.EmployeeDtoDataBaseRepository;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 public interface EmployeeRepository {
-    public Employee create(Employee employee);
+    public EmployeeDtoDataBaseRepository create(Employee employee);
     public Employee readByPid(int pid);
 }
