b39b566662e27556b4c3b9d72147dd6cbb5578eb
KDH <kde6571@gmail.com>
2023-11-14 10:54:43 +0900

    Feat: Add RequestMapping front of EmployeeControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/EmployeeControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/EmployeeControllerApi.java
@@ -3,9 +3,11 @@ package kr.ac.kmu.dbp.controller.api;
 import kr.ac.kmu.dbp.service.employee.EmployeeService;
 import kr.ac.kmu.dbp.service.employee.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeControllerApi {
     private final EmployeeService employeeService;
 
