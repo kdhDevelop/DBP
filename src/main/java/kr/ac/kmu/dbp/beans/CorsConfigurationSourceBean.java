1816e7bc7fc69f2a559d97329b7d8558ea06d150
KDH <kde6571@gmail.com>
2023-11-14 19:56:00 +0900

    Feat: Add CorsConfigurationSourceBean

---
+++ src/main/java/kr/ac/kmu/dbp/beans/CorsConfigurationSourceBean.java
@@ -0,0 +1,26 @@
+package kr.ac.kmu.dbp.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Component
+public class CorsConfigurationSourceBean {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
