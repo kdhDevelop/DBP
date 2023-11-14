b8c79337e2da9cc62619a5180ecdd0ac8e4056c1
KDH <kde6571@gmail.com>
2023-11-14 11:00:07 +0900

    Feat: Add create endpoint in DepartmentControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
@@ -1,8 +1,11 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
 import kr.ac.kmu.dbp.service.department.DepartmentService;
 import kr.ac.kmu.dbp.service.department.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,10 @@ public class DepartmentControllerApi {
     @Autowired
     public DepartmentControllerApi(DepartmentServiceImpl departmentServiceImpl) {
         this.departmentService = departmentServiceImpl;
+    }
+
+    @PostMapping("/department")
+    public void create(@RequestBody DepartmentDtoCreate departmentDtoCreate) {
+        departmentService.create(departmentDtoCreate);
     }
 }
