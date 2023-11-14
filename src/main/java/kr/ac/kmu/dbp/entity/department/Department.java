5cf3c1f265552edc4225abf1c47a1158a2f716ee
KDH <kde6571@gmail.com>
2023-11-14 09:07:47 +0900

    Fix: Add AllArgsConstructor Annotation in Department

--- src/main/java/kr/ac/kmu/dbp/entity/department/Department.java
+++ src/main/java/kr/ac/kmu/dbp/entity/department/Department.java
@@ -2,11 +2,13 @@ package kr.ac.kmu.dbp.entity.department;
 
 import kr.ac.kmu.dbp.dto.department.DepartmentDtoRepository;
 import kr.ac.kmu.dbp.entity.employee.Employee;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Department {
     private int pid;
     private String name;
