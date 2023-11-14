5ed870a30b725c22d151faad238c63dc51ce75c5
KDH <kde6571@gmail.com>
2023-11-14 11:06:55 +0900

    Feat: Add create method in EmployeeService

--- src/main/java/kr/ac/kmu/dbp/service/employee/EmployeeService.java
+++ src/main/java/kr/ac/kmu/dbp/service/employee/EmployeeService.java
@@ -1,9 +1,12 @@
 package kr.ac.kmu.dbp.service.employee;
 
+import kr.ac.kmu.dbp.dto.employee.EmployeeDtoCreate;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 public interface EmployeeService {
     public Employee readByPid(int pid);
 
     public Employee readByAccount(String account);
+
+    public void create(EmployeeDtoCreate employeeDtoCreate);
 }
