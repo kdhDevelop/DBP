cd76c98d55bce880b1391600ef64057d7f89e497
KDH <kde6571@gmail.com>
2023-11-14 12:35:57 +0900

    Feat: Add EmployeeDtoRead

---
+++ src/main/java/kr/ac/kmu/dbp/dto/employee/EmployeeDtoRead.java
@@ -0,0 +1,42 @@
+package kr.ac.kmu.dbp.dto.employee;
+
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
+public class EmployeeDtoRead {
+
+    private int pid;
+
+    private String account;
+    private String name;
+    private String gender;
+
+    private String phoneNumber;
+
+    private long zipCode;
+    private String address1;
+    private String address2;
+
+    private String role;
+    private String rank;
+
+    private int departmentPid;
+
+    public EmployeeDtoRead(Employee employee) {
+        this.pid = employee.getPid();
+
+        this.account = employee.getAccount();
+        this.name = employee.getName();
+        this.gender = employee.getGender().name();
+
+        this.phoneNumber = employee.getPhoneNumber();
+
+        this.zipCode = employee.getZipCode();
+        this.address1 = employee.getAddress1();
+        this.address2 = employee.getAddress2();
+
+        this.role = employee.getRole().name();
+        this.rank = employee.getRank().name();
+
+        this.departmentPid = employee.getDepartment().getPid();
+    }
+}
