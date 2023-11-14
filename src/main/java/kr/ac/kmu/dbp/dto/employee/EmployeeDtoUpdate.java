5c712e52c9e28e381274c846d9c981606f473758
KDH <kde6571@gmail.com>
2023-11-14 12:44:13 +0900

    Feat: Add EmployeeDtoUpdate

---
+++ src/main/java/kr/ac/kmu/dbp/dto/employee/EmployeeDtoUpdate.java
@@ -0,0 +1,24 @@
+package kr.ac.kmu.dbp.dto.employee;
+
+import kr.ac.kmu.dbp.entity.department.Department;
+import kr.ac.kmu.dbp.entity.employee.Gender;
+import kr.ac.kmu.dbp.entity.employee.Rank;
+import kr.ac.kmu.dbp.entity.employee.Role;
+import lombok.Getter;
+
+@Getter
+public class EmployeeDtoUpdate {
+    private String name;
+    private String gender;
+
+    private String phoneNumber;
+
+    private long zipCode;
+    private String address1;
+    private String address2;
+
+    private String role;
+    private String rank;
+
+    private String departmentPid;
+}
