f7cb450480a279f11b491a4fd7ecdf1906864dfc
KDH <kde6571@gmail.com>
2023-11-14 10:54:11 +0900

    Feat: Add RequestMapping front of DepartmentControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
@@ -3,9 +3,11 @@ package kr.ac.kmu.dbp.controller.api;
 import kr.ac.kmu.dbp.service.department.DepartmentService;
 import kr.ac.kmu.dbp.service.department.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class DepartmentControllerApi {
     private final DepartmentService departmentService;
 
