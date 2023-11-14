e29e5a98290ddb8d216e1a913f37e53b4392c6ce
KDH <kde6571@gmail.com>
2023-11-14 20:10:29 +0900

    Feat: Add redirect example endpoint

--- src/main/java/kr/ac/kmu/dbp/controller/api/IndexControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/IndexControllerApi.java
@@ -1,12 +1,24 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class IndexControllerApi {
 
     @GetMapping("/")
     public void index() {
+    }
+
+    @GetMapping("/redirect")
+    public ResponseEntity<?> redirect() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create("/"));
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 }
