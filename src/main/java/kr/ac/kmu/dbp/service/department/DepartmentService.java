2182a4ee6347fe9bac16714d09a2e99638687e56
KDH <kde6571@gmail.com>
2023-11-14 10:56:49 +0900

    Feat: Add create method in DepartmentService

--- src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
+++ src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
@@ -1,8 +1,10 @@
 package kr.ac.kmu.dbp.service.department;
 
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
 import kr.ac.kmu.dbp.entity.department.Department;
 
 public interface DepartmentService {
 
     public Department readByPid(int pid);
+    public void create(DepartmentDtoCreate departmentDtoCreate);
 }
