2e2c26f1720c8678ee4a5933502d6e07c114525e
KDH <kde6571@gmail.com>
2023-11-14 10:58:16 +0900

    Feat: Add constructor for DepartmentDtoCreate in Department

--- src/main/java/kr/ac/kmu/dbp/entity/department/Department.java
+++ src/main/java/kr/ac/kmu/dbp/entity/department/Department.java
@@ -1,5 +1,6 @@
 package kr.ac.kmu.dbp.entity.department;
 
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,9 @@ public class Department {
     public Department(ResultSet resultSet) throws SQLException {
         this.pid = resultSet.getInt("depPid");
         this.name = resultSet.getString("depName");
+    }
+
+    public Department(DepartmentDtoCreate departmentDtoCreate) {
+        this.name = departmentDtoCreate.getName();
     }
 }
