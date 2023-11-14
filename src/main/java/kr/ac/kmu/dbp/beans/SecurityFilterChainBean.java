c9798fbf1beee5b4d2df4bf108ab00515a41e497
KDH <kde6571@gmail.com>
2023-11-14 19:57:40 +0900

    Fix: Change cors setting

--- src/main/java/kr/ac/kmu/dbp/beans/SecurityFilterChainBean.java
+++ src/main/java/kr/ac/kmu/dbp/beans/SecurityFilterChainBean.java
@@ -1,20 +1,27 @@
 package kr.ac.kmu.dbp.beans;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityFilterChainBean {
+
+    @Autowired
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
-                .cors().disable();
+                .cors().configurationSource(corsConfigurationSource);
 
         httpSecurity
                 .formLogin()
