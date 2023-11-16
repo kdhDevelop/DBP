91b5b843eb71359b14e5b912cf18503217fc5629
KDH <kde6571@gmail.com>
2023-11-17 00:04:58 +0900

    Feat: Add create method in ChatRoomRepository

--- src/main/java/kr/ac/kmu/dbp/repository/chat/chatRoom/ChatRoomRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/chat/chatRoom/ChatRoomRepository.java
@@ -1,4 +1,7 @@
 package kr.ac.kmu.dbp.repository.chat.chatRoom;
 
+import kr.ac.kmu.dbp.entity.chat.chatRoom.ChatRoom;
+
 public interface ChatRoomRepository {
+    public void create(ChatRoom chatRoom);
 }
