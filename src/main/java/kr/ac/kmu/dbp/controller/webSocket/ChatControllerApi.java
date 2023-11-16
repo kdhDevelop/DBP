7c8a5883c9494fcfbab4a907ce1064714b1a9a7f
KDH <kde6571@gmail.com>
2023-11-17 07:54:38 +0900

    Feat: Add create method in ChatControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/webSocket/ChatControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/webSocket/ChatControllerApi.java
@@ -1,8 +1,11 @@
 package kr.ac.kmu.dbp.controller.webSocket;
 
+import kr.ac.kmu.dbp.dto.chat.chatRoom.ChatRoomDtoCreate;
 import kr.ac.kmu.dbp.service.chat.chatRoom.ChatRoomService;
 import kr.ac.kmu.dbp.service.chat.chatRoom.ChatRoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +16,10 @@ public class ChatControllerApi {
     @Autowired
     public ChatControllerApi(ChatRoomServiceImpl chatRoomServiceImpl) {
         this.chatRoomService = chatRoomServiceImpl;
+    }
+
+    @PostMapping("/chat")
+    public void create(@RequestBody ChatRoomDtoCreate chatRoomDtoCreate) {
+        chatRoomService.create(chatRoomDtoCreate);
     }
 }
