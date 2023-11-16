1a2b0bada2aa53f1ba3b87c1c4a872cc77d93aeb
KDH <kde6571@gmail.com>
2023-11-16 14:32:32 +0900

    Feat: Add Random Function in Gender

--- src/main/java/kr/ac/kmu/dbp/entity/employee/Gender.java
+++ src/main/java/kr/ac/kmu/dbp/entity/employee/Gender.java
@@ -1,5 +1,9 @@
 package kr.ac.kmu.dbp.entity.employee;
 
 public enum Gender {
-    남성, 여성
+    남성, 여성;
+
+    public static Gender getRandom() {
+        return values()[(int) (Math.random()*values().length)];
+    }
 }
