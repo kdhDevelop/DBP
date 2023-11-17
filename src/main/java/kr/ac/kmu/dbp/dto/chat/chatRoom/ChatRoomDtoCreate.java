7b7592ac3d841d5f63ee159f8ccee84c5f287201
KDH <kde6571@gmail.com>
2023-11-17 12:58:21 +0900

    Feat: Add NoArgsConstructor in ChatRoomDtoCreate

--- src/main/java/kr/ac/kmu/dbp/dto/chat/chatRoom/ChatRoomDtoCreate.java
+++ src/main/java/kr/ac/kmu/dbp/dto/chat/chatRoom/ChatRoomDtoCreate.java
@@ -3,9 +3,11 @@ package kr.ac.kmu.dbp.dto.chat.chatRoom;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ChatRoomDtoCreate {
     private String name;
