8e3c3d23e09f98193671ae5a48937d10ce44a809
KDH <kde6571@gmail.com>
2023-11-17 12:59:01 +0900

    Feat: Add RequestMapping for /api in ChatControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/webSocket/ChatControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/webSocket/ChatControllerApi.java
@@ -6,9 +6,11 @@ import kr.ac.kmu.dbp.service.chat.chatRoom.ChatRoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ChatControllerApi {
 
     private final ChatRoomService chatRoomService;
