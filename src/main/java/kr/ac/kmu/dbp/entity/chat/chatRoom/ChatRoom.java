919044e0f48a94cc8b4f0bc87da9f93ddd1cd5ee
KDH <kde6571@gmail.com>
2023-11-17 10:09:57 +0900

    Fix: Change userPid List to Employee List in ChatRoom

--- src/main/java/kr/ac/kmu/dbp/entity/chat/chatRoom/ChatRoom.java
+++ src/main/java/kr/ac/kmu/dbp/entity/chat/chatRoom/ChatRoom.java
@@ -1,6 +1,7 @@
 package kr.ac.kmu.dbp.entity.chat.chatRoom;
 
 import kr.ac.kmu.dbp.dto.chat.chatRoom.ChatRoomDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ public class ChatRoom {
 
     private int pid;
     private String name;
-    private final List<Integer> joinUserPidList = new ArrayList<>();
+    private final List<Employee> joinEmployeeList = new ArrayList<>();
 
     public ChatRoom(int pid, String name) {
         this.pid = pid;
@@ -26,15 +27,15 @@ public class ChatRoom {
         this.name = chatRoomDtoCreate.getName();
     }
 
-    public void addUserPid(int userPid) {
-        joinUserPidList.add(userPid);
+    public void addEmployee(Employee employee) {
+        joinEmployeeList.add(employee);
     }
 
-    public void removeUserPid(int userPid) {
-        joinUserPidList.remove(userPid);
+    public void removeEmployee(Employee employee) {
+        joinEmployeeList.remove(employee);
     }
 
-    public List<Integer> getUserPidList() {
-        return joinUserPidList;
+    public List<Employee> getEmployeeList() {
+        return joinEmployeeList;
     }
 }
