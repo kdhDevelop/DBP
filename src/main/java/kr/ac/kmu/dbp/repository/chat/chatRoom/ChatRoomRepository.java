f23c4e5a92e0d84f3df3837b8e70e84e81e9a694
KDH <kde6571@gmail.com>
2023-11-17 13:00:53 +0900

    Feat: Add readAll method in ChatRoomRepository

--- src/main/java/kr/ac/kmu/dbp/repository/chat/chatRoom/ChatRoomRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/chat/chatRoom/ChatRoomRepository.java
@@ -2,7 +2,10 @@ package kr.ac.kmu.dbp.repository.chat.chatRoom;
 
 import kr.ac.kmu.dbp.entity.chat.chatRoom.ChatRoom;
 
+import java.util.List;
+
 public interface ChatRoomRepository {
     public void create(ChatRoom chatRoom);
     public ChatRoom readByPid(int pid);
+    public List<ChatRoom> readAll();
 }
