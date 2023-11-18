6000a64e528facd093183687a6b8586be417aeba
KDH <kde6571@gmail.com>
2023-11-18 15:23:56 +0900

    Feat: Add update method in DepartmentService

--- src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
+++ src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
@@ -2,6 +2,7 @@ package kr.ac.kmu.dbp.service.department;
 
 import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
 import kr.ac.kmu.dbp.dto.department.DepartmentDtoDelete;
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoUpdate;
 import kr.ac.kmu.dbp.entity.department.Department;
 
 public interface DepartmentService {
@@ -9,4 +10,5 @@ public interface DepartmentService {
     public Department readByPid(int pid);
     public void create(DepartmentDtoCreate departmentDtoCreate);
     public void delete(DepartmentDtoDelete departmentDtoDelete);
+    public void update(DepartmentDtoUpdate departmentDtoUpdate);
 }
