184c16a21c9b4a63334a1b9f48f4336edd1a723b
KDH <kde6571@gmail.com>
2023-11-18 15:13:32 +0900

    Feat: Add delete method in DepartmentService

--- src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
+++ src/main/java/kr/ac/kmu/dbp/service/department/DepartmentService.java
@@ -1,10 +1,12 @@
 package kr.ac.kmu.dbp.service.department;
 
 import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoDelete;
 import kr.ac.kmu.dbp.entity.department.Department;
 
 public interface DepartmentService {
 
     public Department readByPid(int pid);
     public void create(DepartmentDtoCreate departmentDtoCreate);
+    public void delete(DepartmentDtoDelete departmentDtoDelete);
 }
