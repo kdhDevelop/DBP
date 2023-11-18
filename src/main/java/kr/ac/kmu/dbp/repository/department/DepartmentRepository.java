eb245c1167bbacba28521a8ef46176445424dcc9
KDH <kde6571@gmail.com>
2023-11-18 15:27:24 +0900

    Feat: Add readAll method in DepartmentRepository

--- src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/department/DepartmentRepository.java
@@ -2,10 +2,13 @@ package kr.ac.kmu.dbp.repository.department;
 
 import kr.ac.kmu.dbp.entity.department.Department;
 
+import java.util.List;
+
 public interface DepartmentRepository {
     public Department create(Department department);
     public Department readByPid(int pid);
     public boolean checkExistByName(String name);
     public void delete(Department department);
     public void update(Department department);
+    public List<Department> readAll();
 }
