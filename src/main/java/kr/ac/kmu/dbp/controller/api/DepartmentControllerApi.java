01aaacf6f3e5651ad0c7bccecc204f39879f1ca1
KDH <kde6571@gmail.com>
2023-11-18 15:35:44 +0900

    Feat: Add update department endpoint

--- src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/DepartmentControllerApi.java
@@ -1,13 +1,11 @@
 package kr.ac.kmu.dbp.controller.api;
 
 import kr.ac.kmu.dbp.dto.department.DepartmentDtoCreate;
+import kr.ac.kmu.dbp.dto.department.DepartmentDtoUpdate;
 import kr.ac.kmu.dbp.service.department.DepartmentService;
 import kr.ac.kmu.dbp.service.department.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,5 +20,10 @@ public class DepartmentControllerApi {
     @PostMapping("/department")
     public void create(@RequestBody DepartmentDtoCreate departmentDtoCreate) {
         departmentService.create(departmentDtoCreate);
+    }
+
+    @PutMapping("/department")
+    public void update(@RequestBody DepartmentDtoUpdate departmentDtoUpdate) {
+        departmentService.update(departmentDtoUpdate);
     }
 }
