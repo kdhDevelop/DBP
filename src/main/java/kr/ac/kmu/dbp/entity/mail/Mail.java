ef80d8cb2cbcf97e5423ca8fab1210faa2bcb627
KDH <kde6571@gmail.com>
2023-11-20 00:49:18 +0900

    Feat: Add Mail

---
+++ src/main/java/kr/ac/kmu/dbp/entity/mail/Mail.java
@@ -0,0 +1,24 @@
+package kr.ac.kmu.dbp.entity.mail;
+
+import kr.ac.kmu.dbp.entity.employee.Employee;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Mail {
+    private int pid;
+
+    private Employee sender;
+    private Date sendDate;
+
+    private Employee receiver;
+    private boolean receipt;
+    private Date receiptDate;
+
+    private String content;
+}
