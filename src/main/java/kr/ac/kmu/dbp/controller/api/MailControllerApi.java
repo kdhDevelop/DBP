6c86ad9f490911f47dc9566605434565e421e716
KDH <kde6571@gmail.com>
2023-11-20 01:18:11 +0900

    Fix: Change date to Timestamp for table create query in MailDataBaseRepository

--- src/main/java/kr/ac/kmu/dbp/controller/api/MailControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/MailControllerApi.java
@@ -1,8 +1,13 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.mail.MailDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.mail.MailService;
 import kr.ac.kmu.dbp.service.mail.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +21,8 @@ public class MailControllerApi {
         this.mailService = mailServiceImpl;
     }
 
+    @PostMapping("/mail")
+    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody MailDtoCreate mailDtoCreate) {
+        mailService.create(customUserDetails.getEmployee(), mailDtoCreate);
+    }
 }
