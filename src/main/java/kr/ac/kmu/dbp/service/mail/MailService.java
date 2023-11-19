511cc0a81112204f23daffc509b78a326b1bbfa8
KDH <kde6571@gmail.com>
2023-11-20 01:25:20 +0900

    Feat: Add readAllInfo method in MailService

--- src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
+++ src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
@@ -1,8 +1,12 @@
 package kr.ac.kmu.dbp.service.mail;
 
 import kr.ac.kmu.dbp.dto.mail.MailDtoCreate;
+import kr.ac.kmu.dbp.dto.mail.MailDtoReadInfo;
 import kr.ac.kmu.dbp.entity.employee.Employee;
+
+import java.util.List;
 
 public interface MailService {
     public void create(Employee sender, MailDtoCreate mailDtoCreate);
+    public List<MailDtoReadInfo> readAllInfo(Employee employee);
 }
