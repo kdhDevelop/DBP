22445ffd10f7ed24e42d0579b25989219971a6ce
KDH <kde6571@gmail.com>
2023-11-20 03:05:39 +0900

    Feat: Add readAllSend method in MailService

--- src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
+++ src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
@@ -3,6 +3,7 @@ package kr.ac.kmu.dbp.service.mail;
 import kr.ac.kmu.dbp.dto.mail.MailDtoCreate;
 import kr.ac.kmu.dbp.dto.mail.MailDtoRead;
 import kr.ac.kmu.dbp.dto.mail.MailDtoReadReceiveInfo;
+import kr.ac.kmu.dbp.dto.mail.MailDtoReadSendInfo;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 public interface MailService {
     public void create(Employee sender, MailDtoCreate mailDtoCreate);
     public List<MailDtoReadReceiveInfo> readAllReceiveInfo(Employee employee);
+    public List<MailDtoReadSendInfo> readAllSendInfo(Employee employee);
     public MailDtoRead readByPid(Employee employee, int pid);
     public boolean checkNewMail(Employee employee);
 }
