032adb37b011aab1ad0dad7b7084b94cb0c5d5c0
KDH <kde6571@gmail.com>
2023-11-20 01:26:03 +0900

    Feat: Add readAll method in MailRepository

--- src/main/java/kr/ac/kmu/dbp/repository/mail/MailRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/mail/MailRepository.java
@@ -1,7 +1,11 @@
 package kr.ac.kmu.dbp.repository.mail;
 
+import kr.ac.kmu.dbp.entity.employee.Employee;
 import kr.ac.kmu.dbp.entity.mail.Mail;
+
+import java.util.List;
 
 public interface MailRepository {
     public void create(Mail mail);
+    public List<Mail> readAll(Employee employee);
 }
