401d008eb07978e76b03ecb2a90911e95c9d5b54
KDH <kde6571@gmail.com>
2023-11-20 01:26:38 +0900

    Feat: Add readAllInfo endpoint in MailControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/MailControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/MailControllerApi.java
@@ -1,15 +1,16 @@
 package kr.ac.kmu.dbp.controller.api;
 
 import kr.ac.kmu.dbp.dto.mail.MailDtoCreate;
+import kr.ac.kmu.dbp.dto.mail.MailDtoReadInfo;
 import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
+import kr.ac.kmu.dbp.entity.mail.Mail;
 import kr.ac.kmu.dbp.service.mail.MailService;
 import kr.ac.kmu.dbp.service.mail.MailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,5 +25,10 @@ public class MailControllerApi {
     @PostMapping("/mail")
     public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody MailDtoCreate mailDtoCreate) {
         mailService.create(customUserDetails.getEmployee(), mailDtoCreate);
+    }
+
+    @GetMapping("/mail")
+    public List<MailDtoReadInfo> readAllInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return mailService.readAllInfo(customUserDetails.getEmployee());
     }
 }
