20eeb9d6e2c5957da3cd67eaf3b3cd5dcbfa4468
KDH <kde6571@gmail.com>
2023-11-20 01:08:38 +0900

    Fix: Add sender for create method in MailService

--- src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
+++ src/main/java/kr/ac/kmu/dbp/service/mail/MailService.java
@@ -1,7 +1,8 @@
 package kr.ac.kmu.dbp.service.mail;
 
 import kr.ac.kmu.dbp.dto.mail.MailDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
 
 public interface MailService {
-    public void create(MailDtoCreate mailDtoCreate);
+    public void create(Employee sender, MailDtoCreate mailDtoCreate);
 }
