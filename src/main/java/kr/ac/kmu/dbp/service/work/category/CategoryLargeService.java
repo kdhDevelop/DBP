5dea3f0127ea2d0c5519f06bd4b31368886edfb7
KDH <kde6571@gmail.com>
2023-11-20 10:00:33 +0900

    Feat: Add create method in CategoryLargeService

--- src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
+++ src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
@@ -1,4 +1,8 @@
 package kr.ac.kmu.dbp.service.work.category;
 
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
 public interface CategoryLargeService {
+    public void create(Employee employee, CategoryLargeDtoCreate categoryLargeDtoCreate);
 }
