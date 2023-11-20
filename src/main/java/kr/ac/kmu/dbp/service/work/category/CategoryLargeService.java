1d4e38cc955913ce18e02d8201c2f0d4f16fe3c4
KDH <kde6571@gmail.com>
2023-11-20 10:10:29 +0900

    Feat: Add update method in CategoryLargeService

--- src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
+++ src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
@@ -1,8 +1,10 @@
 package kr.ac.kmu.dbp.service.work.category;
 
 import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoUpdate;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 public interface CategoryLargeService {
     public void create(Employee employee, CategoryLargeDtoCreate categoryLargeDtoCreate);
+    public void update(Employee employee, CategoryLargeDtoUpdate categoryLargeDtoUpdate);
 }
