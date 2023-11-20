261fb9d8912cc07ee0e56e809f5320db095c2495
KDH <kde6571@gmail.com>
2023-11-20 18:20:44 +0900

    Feat: Add readAll method in EmployeeRepository

--- src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/employee/EmployeeRepository.java
@@ -2,10 +2,13 @@ package kr.ac.kmu.dbp.repository.employee;
 
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
+import java.util.List;
+
 public interface EmployeeRepository {
     public Employee create(Employee employee);
     public Employee readByPid(int pid);
     public Employee readByAccount(String account);
+    public List<Employee> readAll();
     public Employee update(Employee employee);
     public boolean checkExistByResidentRegistrationNumber(String residentRegistrationNumber);
 }
