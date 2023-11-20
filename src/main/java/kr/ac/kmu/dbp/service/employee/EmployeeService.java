6b90de501ddb01eee82bd5e3703ae764cceb8cfa
KDH <kde6571@gmail.com>
2023-11-20 18:21:20 +0900

    Feat: Add readAllAccount method in EmployeeService

--- src/main/java/kr/ac/kmu/dbp/service/employee/EmployeeService.java
+++ src/main/java/kr/ac/kmu/dbp/service/employee/EmployeeService.java
@@ -4,10 +4,14 @@ import kr.ac.kmu.dbp.dto.employee.EmployeeDtoCreate;
 import kr.ac.kmu.dbp.dto.employee.EmployeeDtoUpdate;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     public Employee readByPid(int pid);
 
     public Employee readByAccount(String account);
+
+    public List<Integer> readAllAccount();
 
     public void create(EmployeeDtoCreate employeeDtoCreate);
 
