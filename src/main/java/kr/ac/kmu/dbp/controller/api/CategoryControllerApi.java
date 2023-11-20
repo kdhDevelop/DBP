dfdc7cf64d6c570e4bb289ac2460eeb4d6641145
KDH <kde6571@gmail.com>
2023-11-20 10:01:01 +0900

    Feat: Add create category large endpoint in CategoryControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/CategoryControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/CategoryControllerApi.java
@@ -1,7 +1,12 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.work.category.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,10 @@ public class CategoryControllerApi {
         this.categoryLargeService = categoryLargeServiceImpl;
         this.categoryMediumService = categoryMediumServiceImpl;
         this.categorySmallService = categorySmallServiceImpl;
+    }
+
+    @PostMapping("/category/large")
+    public void createLarge(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CategoryLargeDtoCreate categoryLargeDtoCreate) {
+        categoryLargeService.create(customUserDetails.getEmployee(), categoryLargeDtoCreate);
     }
 }
