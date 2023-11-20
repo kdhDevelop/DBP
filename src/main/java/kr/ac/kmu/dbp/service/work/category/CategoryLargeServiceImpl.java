82e2ef08feef6c92eaaba85bca87c80dfab17915
KDH <kde6571@gmail.com>
2023-11-20 10:00:41 +0900

    Feat: Implementation create method in CategoryLargeServiceImpl

--- src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeServiceImpl.java
+++ src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeServiceImpl.java
@@ -1,5 +1,9 @@
 package kr.ac.kmu.dbp.service.work.category;
 
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+import kr.ac.kmu.dbp.entity.employee.Role;
+import kr.ac.kmu.dbp.entity.work.category.CategoryLarge;
 import kr.ac.kmu.dbp.repository.work.category.CategoryLargeDataBaseRepository;
 import kr.ac.kmu.dbp.repository.work.category.CategoryLargeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +16,14 @@ public class CategoryLargeServiceImpl implements CategoryLargeService {
     @Autowired
     public CategoryLargeServiceImpl(CategoryLargeDataBaseRepository categoryLargeDataBaseRepository) {
         this.categoryLargeRepository = categoryLargeDataBaseRepository;
+    }
+
+    @Override
+    public void create(Employee employee, CategoryLargeDtoCreate categoryLargeDtoCreate) {
+        if (employee.getRole() == Role.부서장 || employee.getRole() == Role.사장) {
+            categoryLargeRepository.create(new CategoryLarge(categoryLargeDtoCreate));
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
