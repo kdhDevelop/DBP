c7056c3cc3c156bf6c3670be4fb043c98b027d4e
KDH <kde6571@gmail.com>
2023-11-20 10:44:00 +0900

    Feat: Add read all method in CategoryLargeRepository

--- src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeRepository.java
@@ -2,8 +2,11 @@ package kr.ac.kmu.dbp.repository.work.category;
 
 import kr.ac.kmu.dbp.entity.work.category.CategoryLarge;
 
+import java.util.List;
+
 public interface CategoryLargeRepository {
     public void create(CategoryLarge categoryLarge);
     public void update(CategoryLarge categoryLarge);
     public void delete(CategoryLarge categoryLarge);
+    public List<CategoryLarge> readAll();
 }
