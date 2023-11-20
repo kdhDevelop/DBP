c9df5def82c3f43224d6b7e3956868ad9bbd9c53
KDH <kde6571@gmail.com>
2023-11-20 10:44:32 +0900

    Feat: Add read all method in CategoryLargeService

--- src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
+++ src/main/java/kr/ac/kmu/dbp/service/work/category/CategoryLargeService.java
@@ -1,11 +1,15 @@
 package kr.ac.kmu.dbp.service.work.category;
 
 import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoRead;
 import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoUpdate;
 import kr.ac.kmu.dbp.entity.employee.Employee;
+
+import java.util.List;
 
 public interface CategoryLargeService {
     public void create(Employee employee, CategoryLargeDtoCreate categoryLargeDtoCreate);
     public void update(Employee employee, CategoryLargeDtoUpdate categoryLargeDtoUpdate);
     public void delete(Employee employee, int pid);
+    public List<CategoryLargeDtoRead> readAll();
 }
