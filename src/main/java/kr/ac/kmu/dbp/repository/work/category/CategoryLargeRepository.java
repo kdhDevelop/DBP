8ea9d911a69d974ea69b945700ea86efa9d4eedf
KDH <kde6571@gmail.com>
2023-11-20 10:17:28 +0900

    Feat: Add delete method in CategoryLargeRepository

--- src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeRepository.java
@@ -5,4 +5,5 @@ import kr.ac.kmu.dbp.entity.work.category.CategoryLarge;
 public interface CategoryLargeRepository {
     public void create(CategoryLarge categoryLarge);
     public void update(CategoryLarge categoryLarge);
+    public void delete(CategoryLarge categoryLarge);
 }
