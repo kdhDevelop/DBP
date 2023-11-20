a5447bf2a9be3cea928cc0db7784c7bc29e77a48
KDH <kde6571@gmail.com>
2023-11-20 09:51:14 +0900

    Feat: Add constructor for CategoryLargeDtoCreate in CategoryLarge

--- src/main/java/kr/ac/kmu/dbp/entity/work/category/CategoryLarge.java
+++ src/main/java/kr/ac/kmu/dbp/entity/work/category/CategoryLarge.java
@@ -1,6 +1,12 @@
 package kr.ac.kmu.dbp.entity.work.category;
 
+import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+
 public class CategoryLarge {
     private int pid;
     private String name;
+
+    public CategoryLarge(CategoryLargeDtoCreate categoryLargeDtoCreate) {
+        this.name = categoryLargeDtoCreate.getName();
+    }
 }
