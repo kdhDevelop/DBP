9831a68fba25e86e2ce4a0b7720963199cea70ec
KDH <kde6571@gmail.com>
2023-11-20 10:00:15 +0900

    Feat: Implementation create method in CategoryLargeDataBaseRepository

--- src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/work/category/CategoryLargeDataBaseRepository.java
@@ -1,9 +1,14 @@
 package kr.ac.kmu.dbp.repository.work.category;
 
+import kr.ac.kmu.dbp.entity.work.category.CategoryLarge;
 import kr.ac.kmu.dbp.repository.DataBaseConnection;
 import kr.ac.kmu.dbp.repository.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Component
 public class CategoryLargeDataBaseRepository extends Table implements CategoryLargeRepository {
@@ -16,5 +21,20 @@ public class CategoryLargeDataBaseRepository extends Table implements CategoryLa
     @Override
     protected String getTableCreateQuery() {
         return "CREATE TABLE categoryLarge ( pid int NOT NULL UNIQUE AUTO_INCREMENT, name varchar(100), disable bool, PRIMARY KEY(pid) );";
+    }
+
+    @Override
+    public void create(CategoryLarge categoryLarge) {
+        try {
+            try (Connection connection = dataBaseConnection.getConnection()) {
+                try (Statement statement = connection.createStatement()) {
+                    String createQuery = "INSERT INTO categoryLarge (name, disable) VALUES ('|=NAME=|', 0);"
+                            .replace("|=NAME=|", categoryLarge.getName());
+                    statement.executeUpdate(createQuery);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 }
