661a3c27a2bfd2fee4d534809def47232d7dfcb2
KDH <kde6571@gmail.com>
2023-11-20 09:51:40 +0900

    Feat: Add Getter Annotation in CategoryLarge

--- src/main/java/kr/ac/kmu/dbp/entity/work/category/CategoryLarge.java
+++ src/main/java/kr/ac/kmu/dbp/entity/work/category/CategoryLarge.java
@@ -1,7 +1,9 @@
 package kr.ac.kmu.dbp.entity.work.category;
 
 import kr.ac.kmu.dbp.dto.work.category.CategoryLargeDtoCreate;
+import lombok.Getter;
 
+@Getter
 public class CategoryLarge {
     private int pid;
     private String name;
