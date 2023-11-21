28637c590b00ac818e807d4d2d15668f1e0533ff
KDH <kde6571@gmail.com>
2023-11-21 20:16:26 +0900

    Feat: Add CRUD endpoint in WorkEnrollControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/WorkEnrollControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/WorkEnrollControllerApi.java
@@ -1,10 +1,18 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoCreate;
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoRead;
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoUpdate;
+import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.work.WorkEnrollService;
 import kr.ac.kmu.dbp.service.work.WorkEnrollServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +22,25 @@ public class WorkEnrollControllerApi {
     @Autowired
     public WorkEnrollControllerApi(WorkEnrollServiceImpl workEnrollServiceImpl) {
         this.workEnrollService = workEnrollServiceImpl;
+    }
+
+    @PostMapping("/work")
+    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody  WorkEnrollDtoCreate workEnrollDtoCreate) {
+        workEnrollService.create(customUserDetails.getEmployee(), workEnrollDtoCreate);
+    }
+
+    @PutMapping("/work")
+    public void update(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody WorkEnrollDtoUpdate workEnrollDtoUpdate) {
+        workEnrollService.update(customUserDetails.getEmployee(), workEnrollDtoUpdate);
+    }
+
+    @DeleteMapping("/work/{pid}")
+    public void delete(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable("pid") String pid) {
+        workEnrollService.delete(customUserDetails.getEmployee(), Integer.parseInt(pid));
+    }
+
+    @GetMapping("/work/all/{date}")
+    public List<WorkEnrollDtoRead> readAll(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable String date) throws ParseException {
+        return workEnrollService.readAll(customUserDetails.getEmployee(), Date.valueOf(date));
     }
 }
