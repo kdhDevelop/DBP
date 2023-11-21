35cc74fd96d1b7d6f46cf893564fff9af26d9903
KDH <kde6571@gmail.com>
2023-11-21 20:15:53 +0900

    Feat: Add CRUD in WorkEnrollService

--- src/main/java/kr/ac/kmu/dbp/service/work/WorkEnrollService.java
+++ src/main/java/kr/ac/kmu/dbp/service/work/WorkEnrollService.java
@@ -1,4 +1,16 @@
 package kr.ac.kmu.dbp.service.work;
 
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoCreate;
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoRead;
+import kr.ac.kmu.dbp.dto.work.WorkEnrollDtoUpdate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
+import java.sql.Date;
+import java.util.List;
+
 public interface WorkEnrollService {
+    public void create(Employee employee, WorkEnrollDtoCreate workEnrollDtoCreate);
+    public void update(Employee employee, WorkEnrollDtoUpdate workEnrollDtoUpdate);
+    public void delete(Employee employee, int pid);
+    public List<WorkEnrollDtoRead> readAll(Employee employee, Date date);
 }
