10d778b2b964b33564828c77c656a11df04ba036
KDH <kde6571@gmail.com>
2023-11-21 20:15:16 +0900

    Feat: Add CRUD in WorkEnrollRepository

--- src/main/java/kr/ac/kmu/dbp/repository/work/WorkEnrollRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/work/WorkEnrollRepository.java
@@ -1,4 +1,14 @@
 package kr.ac.kmu.dbp.repository.work;
 
+import kr.ac.kmu.dbp.entity.employee.Employee;
+import kr.ac.kmu.dbp.entity.work.WorkEnroll;
+
+import java.util.Date;
+import java.util.List;
+
 public interface WorkEnrollRepository {
+    public void create(WorkEnroll workEnroll);
+    public void update(WorkEnroll workEnroll);
+    public void delete(WorkEnroll workEnroll);
+    public List<WorkEnroll> readByEmployeeAndDateOrderByStartWork(Employee employee, Date date);
 }
