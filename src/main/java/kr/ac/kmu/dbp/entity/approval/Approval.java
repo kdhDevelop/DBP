ed925a8e8bd44c6254fd4face642912a8beeb228
KDH <kde6571@gmail.com>
2023-11-23 19:05:45 +0900

    Feat: Add constructor for ResultSet

--- src/main/java/kr/ac/kmu/dbp/entity/approval/Approval.java
+++ src/main/java/kr/ac/kmu/dbp/entity/approval/Approval.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 
 @Data
@@ -30,4 +32,25 @@ public class Approval {
     private boolean secondApproval;
     private Timestamp secondApprovalDateTime;
     private String secondApprovalNote;
+
+    public Approval(ResultSet resultSet, String approvalPrefix, String categorySmallPrefix, String categoryMediumPrefix, String categoryLargePrefix, String drafterEmployeePrefix, String drafterDepartmentPrefix, String firstApprovalEmployeePrefix, String firstDepartmentPrefix, String secondApprovalEmployeePrefix, String secondDepartmentPrefix) throws SQLException {
+        this.pid = resultSet.getInt(approvalPrefix + "pid");
+
+        this.title = resultSet.getString(approvalPrefix + "title");
+        this.content = resultSet.getString(approvalPrefix + "content");
+        this.categorySmall = new CategorySmall(resultSet, categorySmallPrefix, categoryMediumPrefix, categoryLargePrefix);
+
+        this.drafterEmployee = new Employee(resultSet, drafterEmployeePrefix, drafterDepartmentPrefix);
+        this.drafterNote = resultSet.getString(approvalPrefix + "drafterNote");
+
+        this.firstApprovalEmployee = new Employee(resultSet, firstApprovalEmployeePrefix, firstDepartmentPrefix);
+        this.firstApproval = resultSet.getBoolean(approvalPrefix + "firstApproval");
+        this.firstApprovalDateTime = resultSet.getTimestamp(approvalPrefix + "firstApprovalDateTime");
+        this.firstApprovalNote = resultSet.getString(approvalPrefix + "firstApprovalNote");
+
+        this.firstApprovalEmployee = new Employee(resultSet, secondApprovalEmployeePrefix, secondDepartmentPrefix);
+        this.secondApproval = resultSet.getBoolean(approvalPrefix + "secondApproval");
+        this.secondApprovalDateTime = resultSet.getTimestamp(approvalPrefix + "secondApprovalDateTime");
+        this.secondApprovalNote = resultSet.getString(approvalPrefix + "secondApprovalNote");
+    }
 }
