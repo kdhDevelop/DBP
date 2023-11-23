fe2f2fb3e2bd77a100c9a9eff10e4c1e5cca250a
KDH <kde6571@gmail.com>
2023-11-23 17:01:08 +0900

    Feat: Implementation create method in ApprovalServiceImpl

--- src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalServiceImpl.java
+++ src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalServiceImpl.java
@@ -1,7 +1,14 @@
 package kr.ac.kmu.dbp.service.approval;
 
+import kr.ac.kmu.dbp.dto.approval.ApprovalDtoCreate;
+import kr.ac.kmu.dbp.entity.approval.Approval;
+import kr.ac.kmu.dbp.entity.employee.Employee;
 import kr.ac.kmu.dbp.repository.approval.ApprovalDataBaseRepository;
 import kr.ac.kmu.dbp.repository.approval.ApprovalRepository;
+import kr.ac.kmu.dbp.repository.employee.EmployeeDataBaseRepository;
+import kr.ac.kmu.dbp.repository.employee.EmployeeRepository;
+import kr.ac.kmu.dbp.repository.work.category.CategorySmallDataBaseRepository;
+import kr.ac.kmu.dbp.repository.work.category.CategorySmallRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +16,26 @@ import org.springframework.stereotype.Service;
 public class ApprovalServiceImpl implements ApprovalService {
 
     private final ApprovalRepository approvalRepository;
+    private final CategorySmallRepository categorySmallRepository;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public ApprovalServiceImpl(ApprovalDataBaseRepository approvalDataBaseRepository) {
+    public ApprovalServiceImpl(ApprovalDataBaseRepository approvalDataBaseRepository, CategorySmallDataBaseRepository categorySmallDataBaseRepository, EmployeeDataBaseRepository employeeDataBaseRepository) {
         this.approvalRepository = approvalDataBaseRepository;
+        this.categorySmallRepository = categorySmallDataBaseRepository;
+        this.employeeRepository = employeeDataBaseRepository;
+    }
+
+    @Override
+    public void create(Employee employee, ApprovalDtoCreate approvalDtoCreate) {
+        Approval approval = Approval.builder()
+                .drafter(employee)
+                .title(approvalDtoCreate.getTitle())
+                .note(approvalDtoCreate.getNote())
+                .content(approvalDtoCreate.getContent())
+                .categorySmall(categorySmallRepository.readByPid(approvalDtoCreate.getCategorySmallPid()))
+                .departmentHead(employeeRepository.readByPid(approvalDtoCreate.getDepartmentHeadPid()))
+                .build();
+        approvalRepository.create(approval);
     }
 }
