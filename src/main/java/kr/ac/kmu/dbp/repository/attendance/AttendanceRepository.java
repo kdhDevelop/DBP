93d636e5d9370687bdb69db09ad02e4c88ed1d7f
KDH <kde6571@gmail.com>
2023-11-23 13:58:38 +0900

    Feat: Add readByEmployee method in AttendanceRepository

--- src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
@@ -1,7 +1,11 @@
 package kr.ac.kmu.dbp.repository.attendance;
 
 import kr.ac.kmu.dbp.entity.attendance.Attendance;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
+import java.util.List;
 
 public interface AttendanceRepository {
     public void create(Attendance attendance);
+    public List<Attendance> readByEmployee(Employee employee);
 }
