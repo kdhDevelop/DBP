4c8aed7fcd6c820c61d6ad1d19dca0cbc1de8f7e
KDH <kde6571@gmail.com>
2023-11-23 13:08:17 +0900

    Feat: Add create method in AttendanceService

--- src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceService.java
+++ src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceService.java
@@ -1,4 +1,8 @@
 package kr.ac.kmu.dbp.service.attendance;
 
+import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
 public interface AttendanceService {
+    public void create(Employee employee, AttendanceDtoCreate attendanceDtoCreate);
 }
