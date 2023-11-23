9bbf76c6f065eee864dc626d7b6124a10001eb13
KDH <kde6571@gmail.com>
2023-11-23 19:06:25 +0900

    Feat: Add readWitByEmployee method in ApprovalService

--- src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
+++ src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
@@ -1,8 +1,12 @@
 package kr.ac.kmu.dbp.service.approval;
 
 import kr.ac.kmu.dbp.dto.approval.ApprovalDtoCreate;
+import kr.ac.kmu.dbp.dto.approval.ApprovalDtoRead;
 import kr.ac.kmu.dbp.entity.employee.Employee;
+
+import java.util.List;
 
 public interface ApprovalService {
     public void create(Employee employee, ApprovalDtoCreate approvalDtoCreate);
+    public List<ApprovalDtoRead> readWaitByEmployee(Employee employee);
 }
