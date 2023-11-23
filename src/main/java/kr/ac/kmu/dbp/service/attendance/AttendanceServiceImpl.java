63195f6647c5c63491e0403b54f587c6141fdd2d
KDH <kde6571@gmail.com>
2023-11-23 13:08:24 +0900

    Feat: Implementation create method in AttendanceServiceImpl

--- src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceServiceImpl.java
+++ src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceServiceImpl.java
@@ -1,5 +1,8 @@
 package kr.ac.kmu.dbp.service.attendance;
 
+import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
+import kr.ac.kmu.dbp.entity.attendance.Attendance;
+import kr.ac.kmu.dbp.entity.employee.Employee;
 import kr.ac.kmu.dbp.repository.attendance.AttendanceDataBaseRepository;
 import kr.ac.kmu.dbp.repository.attendance.AttendanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +15,17 @@ public class AttendanceServiceImpl implements AttendanceService {
     @Autowired
     public AttendanceServiceImpl(AttendanceDataBaseRepository attendanceDataBaseRepository) {
         this.attendanceRepository = attendanceDataBaseRepository;
+    }
+
+    @Override
+    public void create(Employee employee, AttendanceDtoCreate attendanceDtoCreate) {
+        Attendance attendance = Attendance.builder()
+                .employee(employee)
+                .attendanceDate(attendanceDtoCreate.getAttendanceDate())
+                .startTime(attendanceDtoCreate.getStartTime())
+                .endTime(attendanceDtoCreate.getEndTime())
+                .wage(employee.getWage())
+                .build();
+        attendanceRepository.create(attendance);
     }
 }
