eebb1934c102a5b3b4751c6d7c39733ecfb01aa8
KDH <kde6571@gmail.com>
2023-11-23 17:00:57 +0900

    Feat: Save create method in ApprovalService

--- src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
+++ src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
@@ -1,4 +1,8 @@
 package kr.ac.kmu.dbp.service.approval;
 
+import kr.ac.kmu.dbp.dto.approval.ApprovalDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.Employee;
+
 public interface ApprovalService {
+    public void create(Employee employee, ApprovalDtoCreate approvalDtoCreate);
 }
