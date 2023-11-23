b2e6a56c29afd615b3243467c8f68447958f349f
KDH <kde6571@gmail.com>
2023-11-23 13:30:39 +0900

    Fix: Add @RequestBody annotation for AttendanceDtoCreate in create method

--- src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
@@ -7,6 +7,7 @@ import kr.ac.kmu.dbp.service.attendance.AttendanceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class AttendanceControllerApi {
     }
 
     @PostMapping("/attendance")
-    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, AttendanceDtoCreate attendanceDtoCreate) {
+    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody AttendanceDtoCreate attendanceDtoCreate) {
         attendanceService.create(customUserDetails.getEmployee(), customUserDetails.getEmployee(), attendanceDtoCreate);
     }
 }
