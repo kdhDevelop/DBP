a0dd300292732b646cb676eee7daddaadec9942b
KDH <kde6571@gmail.com>
2023-11-23 13:07:51 +0900

    Feat: Add create method in AttendanceRepository

--- src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
@@ -1,4 +1,7 @@
 package kr.ac.kmu.dbp.repository.attendance;
 
+import kr.ac.kmu.dbp.entity.attendance.Attendance;
+
 public interface AttendanceRepository {
+    public void create(Attendance attendance);
 }
