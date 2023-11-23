afd67cae03a7aa3f7ec84acfd0db091005ece0a5
KDH <kde6571@gmail.com>
2023-11-23 17:02:48 +0900

    Feat: Add create method in ApprovalControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/ApprovalControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/ApprovalControllerApi.java
@@ -1,8 +1,13 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.approval.ApprovalDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.approval.ApprovalService;
 import kr.ac.kmu.dbp.service.approval.ApprovalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,10 @@ public class ApprovalControllerApi {
     @Autowired
     public ApprovalControllerApi(ApprovalServiceImpl approvalServiceImpl) {
         this.approvalService = approvalServiceImpl;
+    }
+
+    @PostMapping("/approval")
+    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ApprovalDtoCreate approvalDtoCreate) {
+        approvalService.create(customUserDetails.getEmployee(), approvalDtoCreate);
     }
 }
