86c621309768ba1ca2b606a2c56f75789dd58444
KDH <kde6571@gmail.com>
2023-11-23 13:59:34 +0900

    Feat: Add readByEmployee endpoint

--- src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
@@ -1,15 +1,15 @@
 package kr.ac.kmu.dbp.controller.api;
 
 import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
+import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoRead;
 import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.attendance.AttendanceService;
 import kr.ac.kmu.dbp.service.attendance.AttendanceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,5 +25,10 @@ public class AttendanceControllerApi {
     @PostMapping("/attendance")
     public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody AttendanceDtoCreate attendanceDtoCreate) {
         attendanceService.create(customUserDetails.getEmployee(), customUserDetails.getEmployee(), attendanceDtoCreate);
+    }
+
+    @GetMapping("/attendance")
+    public List<AttendanceDtoRead> readAll(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        return attendanceService.readByEmployee(customUserDetails.getEmployee(), customUserDetails.getEmployee());
     }
 }
