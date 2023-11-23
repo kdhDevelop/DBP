08f720f5f6364e2faed0b50d55c924b8a04b2078
KDH <kde6571@gmail.com>
2023-11-23 20:34:07 +0900

    Feat: Add updateApproval method in ApprovalService

--- src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
+++ src/main/java/kr/ac/kmu/dbp/service/approval/ApprovalService.java
@@ -2,11 +2,13 @@ package kr.ac.kmu.dbp.service.approval;
 
 import kr.ac.kmu.dbp.dto.approval.ApprovalDtoCreate;
 import kr.ac.kmu.dbp.dto.approval.ApprovalDtoRead;
+import kr.ac.kmu.dbp.dto.approval.ApprovalDtoUpdate;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 import java.util.List;
 
 public interface ApprovalService {
     public void create(Employee employee, ApprovalDtoCreate approvalDtoCreate);
+    public void update(Employee employee, ApprovalDtoUpdate approvalDtoUpdate, String state);
     public List<ApprovalDtoRead> readWaitByEmployee(Employee employee);
 }
