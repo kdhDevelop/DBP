74f2ae7a3c9998062637f609d4117065abdbf8f3
KDH <kde6571@gmail.com>
2023-11-23 13:18:31 +0900

    Fix: Renew function interface for create

--- src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
@@ -1,8 +1,12 @@
 package kr.ac.kmu.dbp.controller.api;
 
+import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
+import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.attendance.AttendanceService;
 import kr.ac.kmu.dbp.service.attendance.AttendanceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +19,10 @@ public class AttendanceControllerApi {
     @Autowired
     public AttendanceControllerApi(AttendanceServiceImpl attendanceServiceImpl) {
         this.attendanceService = attendanceServiceImpl;
+    }
+
+    @PostMapping("/attendance")
+    public void create(@AuthenticationPrincipal CustomUserDetails customUserDetails, AttendanceDtoCreate attendanceDtoCreate) {
+        attendanceService.create(customUserDetails.getEmployee(), customUserDetails.getEmployee(), attendanceDtoCreate);
     }
 }
