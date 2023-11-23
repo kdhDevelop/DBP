ac3532b57ddb3f33e3d670e42de933b0e66008c8
KDH <kde6571@gmail.com>
2023-11-23 13:58:46 +0900

    Feat: Implementation readByEmployee method in AttendanceRepository

--- src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceDataBaseRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceDataBaseRepository.java
@@ -1,14 +1,18 @@
 package kr.ac.kmu.dbp.repository.attendance;
 
 import kr.ac.kmu.dbp.entity.attendance.Attendance;
+import kr.ac.kmu.dbp.entity.employee.Employee;
 import kr.ac.kmu.dbp.repository.DataBaseConnection;
 import kr.ac.kmu.dbp.repository.Table;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AttendanceDataBaseRepository extends Table implements AttendanceRepository {
@@ -37,6 +41,27 @@ public class AttendanceDataBaseRepository extends Table implements AttendanceRep
                             .replace("|=WAGE=|", String.valueOf(attendance.getWage()));
                     System.out.println("CREATE ATTENDANCE QUERY : " + createQuery);
                     statement.executeUpdate(createQuery);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public List<Attendance> readByEmployee(Employee employee) {
+        try {
+            try (Connection connection = dataBaseConnection.getConnection()) {
+                try (Statement statement = connection.createStatement()) {
+                    List<Attendance> result = new ArrayList<>();
+                    String readQuery = "SELECT atte.attendanceDate as atte_attendanceDate, atte.dayOfWeek as atte_dayOfWeek, atte.startTime as atte_startTime, atte.endTime as atte_endTime,  atte.wage as atte_wage, emp.pid as emp_pid, emp.account as emp_account, emp.password as emp_password, emp.name as emp_name, emp.gender as emp_gender, emp.birthYear as emp_birthYear, emp.wage as emp_wage, emp.residentRegistrationNumber as emp_residentRegistrationNumber, emp.phoneNumber as emp_phoneNumber, emp.zipCode as emp_zipCode, emp.address1 as emp_address1, emp.address2 as emp_address2, emp.role as emp_role, emp.rank as emp_rank, dep.pid as dep_pid, dep.name as dep_name FROM  attendance as atte, employee as emp, department as dep WHERE atte.employeePid = emp.pid AND emp.departmentPid = dep.pid AND emp.pid = |=EMPLOYEE_PID=|;"
+                            .replace("|=EMPLOYEE_PID=|", String.valueOf(employee.getPid()));
+                    try (ResultSet resultSet = statement.executeQuery(readQuery)) {
+                        while (resultSet.next()) {
+                            result.add(new Attendance(resultSet, "atte_", "emp_", "dep_"));
+                        }
+                    }
+                    return result;
                 }
             }
         } catch (SQLException e) {
