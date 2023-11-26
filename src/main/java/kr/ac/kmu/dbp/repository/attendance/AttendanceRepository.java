7b095576b0f1df578bc08ba926fe0620f62f8ec3
KDH <kde6571@gmail.com>
2023-11-26 23:25:50 +0900

    Feat: Add readByEmployeeAndBetweenAttendanceDate method in AttendanceRepository

--- src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
+++ src/main/java/kr/ac/kmu/dbp/repository/attendance/AttendanceRepository.java
@@ -3,10 +3,12 @@ package kr.ac.kmu.dbp.repository.attendance;
 import kr.ac.kmu.dbp.entity.attendance.Attendance;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
+import java.util.Date;
 import java.util.List;
 
 public interface AttendanceRepository {
     public void create(Attendance attendance);
     public List<Attendance> readByEmployee(Employee employee);
     public boolean checkExistByEmployeeAndAttendanceDate(Attendance attendance);
+    public List<Attendance> readByEmployeeAndBetweenAttendanceDate(Employee employee, Date startDate, Date endDate);
 }
