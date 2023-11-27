3388b013677d07f4d7bd684db53298eaad42e351
KDH <kde6571@gmail.com>
2023-11-27 09:21:35 +0900

    Feat: Add readByEmployeeAndYearAndMonth method in AttendanceService

--- src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceService.java
+++ src/main/java/kr/ac/kmu/dbp/service/attendance/AttendanceService.java
@@ -1,7 +1,6 @@
 package kr.ac.kmu.dbp.service.attendance;
 
-import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
-import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoRead;
+import kr.ac.kmu.dbp.dto.attendance.*;
 import kr.ac.kmu.dbp.entity.employee.Employee;
 
 import java.util.List;
@@ -10,4 +9,5 @@ public interface AttendanceService {
     public void create(Employee employee, AttendanceDtoCreate attendanceDtoCreate);
     public List<AttendanceDtoRead> readByEmployee(Employee reader, Employee target);
     public List<AttendanceDtoRead> readByEmployeeAndYearAndMonth(Employee employee, int year, int month);
+    public AttendanceWorkSalaryDtoRead calculateWorkSalaryByEmployeeAndYearAndMonth(Employee employee, int year, int month, AttendanceWageMultiple attendanceWageMultiple);
 }
