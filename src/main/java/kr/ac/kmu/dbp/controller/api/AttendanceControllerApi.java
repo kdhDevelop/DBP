016090f1c43f403bdf981caf4a819c87c75f04b1
KDH <kde6571@gmail.com>
2023-11-27 09:23:14 +0900

    Feat: Add calculateSalaryByEmployeeAndYearAndMonthAndMultiple endpoint in AttendanceControllerApi

--- src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
+++ src/main/java/kr/ac/kmu/dbp/controller/api/AttendanceControllerApi.java
@@ -2,6 +2,8 @@ package kr.ac.kmu.dbp.controller.api;
 
 import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoCreate;
 import kr.ac.kmu.dbp.dto.attendance.AttendanceDtoRead;
+import kr.ac.kmu.dbp.dto.attendance.AttendanceWageMultiple;
+import kr.ac.kmu.dbp.dto.attendance.AttendanceWorkSalaryDtoRead;
 import kr.ac.kmu.dbp.entity.employee.customUserDetails.CustomUserDetails;
 import kr.ac.kmu.dbp.service.attendance.AttendanceService;
 import kr.ac.kmu.dbp.service.attendance.AttendanceServiceImpl;
@@ -31,4 +33,10 @@ public class AttendanceControllerApi {
     public List<AttendanceDtoRead> readByYearAndMonth(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam String year, @RequestParam String month) {
         return attendanceService.readByEmployeeAndYearAndMonth(customUserDetails.getEmployee(), Integer.parseInt(year), Integer.parseInt(month));
     }
+
+    @GetMapping("/attendance/salary")
+    public AttendanceWorkSalaryDtoRead calculateSalaryByEmployeeAndYearAndMonthAndMultiple(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestParam String year, @RequestParam String month, @RequestParam String overWorkMultiple, @RequestParam String nightWorkMultiple, @RequestParam String holidayWorkMultiple) {
+        return attendanceService.calculateWorkSalaryByEmployeeAndYearAndMonth(customUserDetails.getEmployee(), Integer.parseInt(year), Integer.parseInt(month), new AttendanceWageMultiple(Double.parseDouble(overWorkMultiple), Double.parseDouble(nightWorkMultiple), Double.parseDouble(holidayWorkMultiple)));
+    }
+
 }
